342ac3e18e6a73ce291d20040e83bbc8ba0163d5
Brandon Bluemner <bluemner@users.noreply.github.com>
2017-10-14 13:25:22 -0500

    multi threading

--- source/PGM.cpp
+++ source/PGM.cpp
@@ -91,6 +91,88 @@ namespace uwm{
 
 	}
 	
+		void PGM::read(
+		const std::string file_path,
+		std::vector<unsigned char> &vector_image,
+		int &x_dimension,
+		int &y_dimension)
+	{
+		FILE *fp;
+		if ((fp=fopen(file_path.c_str(), "rb"))==NULL){
+			printf("read error...\n");
+			exit(-1);
+			}	
+		int maxraw;
+		int c;
+		int i,j;
+		int val;
+		unsigned char *line;
+		char buf[1024];
+		unsigned char *image;
+	
+		while ((c=fgetc(fp)) == '#')
+			fgets(buf, 1024, fp);
+		 ungetc(c, fp);
+		 if (fscanf(fp, "P%d\n", &c) != 1) {
+		   printf ("read error ....");
+		   exit(0);
+		 }
+		 if (c != 5 && c != 2) {
+		   printf ("read error ....");
+		   exit(0);
+		 }
+	
+		 if (c==5) {
+		   while ((c=fgetc(fp)) == '#')
+			 fgets(buf, 1024, fp);
+		   ungetc(c, fp);
+		   if (fscanf(fp, "%d%d%d",&x_dimension, &y_dimension, &maxraw) != 3) {
+			 printf("failed to read width/height/max\n");
+			 exit(0);
+		   }
+		   printf("Width=%d, Height=%d \nMaximum=%d\n",x_dimension,y_dimension,maxraw);
+		   vector_image.resize(x_dimension*y_dimension);
+		   image = (unsigned char*)malloc(sizeof(unsigned char)*x_dimension*y_dimension);
+		   getc(fp);
+	
+		   line = (unsigned char *)malloc(sizeof(unsigned char)*x_dimension);
+		   for (j=0; j<y_dimension; j++) {
+			  fread(line, 1, x_dimension, fp);
+			  for (i=0; i<x_dimension; i++) {
+				image[j*x_dimension+i] = line[i];
+				vector_image.at(j*x_dimension+i) = line[i];
+			 }
+		   }
+		   free(line);
+		 }
+	
+		 else if (c==2) {
+		   while ((c=fgetc(fp)) == '#')
+			 fgets(buf, 1024, fp);
+		   ungetc(c, fp);
+		   if (fscanf(fp, "%d%d%d", &x_dimension, &y_dimension, &maxraw) != 3) {
+			 printf("failed to read width/height/max\n");
+			 exit(0);
+		   }
+		   printf("Width=%d, Height=%d \nMaximum=%d,\n",x_dimension,y_dimension,maxraw);
+	
+		   image = (unsigned char*)malloc(sizeof(unsigned char)*x_dimension*y_dimension);
+		   getc(fp);
+	
+		   for (j=0; j<y_dimension; j++)
+			 for (i=0; i<x_dimension; i++) {
+				fscanf(fp, "%d", &val);
+				image[j*x_dimension+i] = val;
+				vector_image.at(j*x_dimension+i) = val;
+			 }
+	
+		 }
+	
+		 fclose(fp);
+		 free(image);
+		 
+
+	}
 	void PGM::write(
 			const std::string file_path,
 			std::vector<unsigned char> &vector_image,
--- source/hw2.cpp
+++ source/hw2.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
-#include <iomanip> 
+#include <iomanip>
+#include <thread>
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include "../headers/PGM.hpp"
-#define T float 
+#define T float
 
-void apply_mask(const int size,
-                T** mask,
-                const int image_height, 
-                const int image_width,
+void apply_mask(const int segment_start, // start of segment 
+                const int segment_size, // segment size
+                const int mask_size, // size of mask
+                T** mask, // mask
+                const int y_dimension, // image height
+                const int x_dimension, // image width
                 const  std::vector<unsigned char> &original,
                 std::vector<unsigned char> &filtered)
 {
-    for(int i =0; i<image_width; i++){
-        for(int j=0; j< image_height; j++){
-            for(int k=0; k < i+size; k++){
-                for(int l=0;l < j+size; l++){
-                    filtered[j*image_width+ i] = mask[k-i][l-j] *filtered[j*image_width+ i];
+    int k_shift = 0;
+    int l_shift = 0;
+    for(int i =0; i<x_dimension; i++){
+        for(int j=segment_start; j< segment_size; j++){
+            for(int k=0; k < i + mask_size; k++){
+                for(int l=0;l < j + mask_size; l++){
+
+                    //filtered[j*image_width+ i] = mask[k-i][l-j] *filtered[j*image_width+ i];
+                    k_shift = i - k / 2;
+                    l_shift = j - l / 2;
+                    if( k < 0 || k >= i){
+                        k=i;
+                    }
+                    if( l < 0 || l <=j ){
+                        l=j;
+                    }
+                    filtered[j*x_dimension+ i] = mask[k][l] * filtered[j*x_dimension +i];
                 }
             }
+            if(i+1 == x_dimension && j+1==segment_size )
+                std::cout<<"Done with Row::"<< j << std::endl;
         }
+       
     }
 }
 
@@ -72,19 +90,19 @@ int main(int argc, char * argv[]){
 	std::string file_path_original = std::string(argv[1]);
     std::string file_path_new = std::string(argv[2]);
     int mask_size  = std::stoi(argv[3]);
-    int threads   = std::stoi(argv[4]);
+    int thread_count   = std::stoi(argv[4]);
     
-    if(mask_size < 1 || threads < 1){
-        std::cerr << "mask_size >=1 & threads >=1" <<std::endl;
+    if(mask_size < 1 || thread_count < 1){
+        std::cerr << "mask_size >=1 & thread_count >=1" <<std::endl;
         return -1;
     }else if(mask_size % 2 == 0){
-        std::cerr << "mask size should be an odd number" <<std::endl;
+        std::cerr << "Mask size should be an odd number" <<std::endl;
         return -1;
     }
 
-    int image_width = 0, image_height = 0;
+    int x_dimension = 0, y_dimension = 0;
     std::vector<unsigned char> image;
-    UWM::PGM().read(file_path_original, image, image_width, image_height);
+    UWM::PGM().read(file_path_original, image, x_dimension, y_dimension);
     std::cout<<"Image Loaded"<< std::endl;
 
     // Memory allocation
@@ -97,21 +115,55 @@ int main(int argc, char * argv[]){
     }
     
     get_gaussian_mask(mask_size, mask);
-
-    std::vector<unsigned char> fi;
-    fi.resize(image_height,image_width);
-    /*
-        const int size,
-                const T** mask,
-                const int image_height, 
-                const int image_width,
-                const  std::vector<unsigned char> &original,
-                std::vector<unsigned char> &filtered
-    */
-    apply_mask(mask_size,mask,image_height, image_width,image, fi);
-
-
     print(mask_size,mask);
+
+    std::vector<unsigned char> fi;  
+    fi.resize(x_dimension * y_dimension);
+    
+
+    std::vector<std::thread> thread_list;
+
+    int segment_size =  y_dimension / thread_count ;
+    int segment_size_adj=  y_dimension / thread_count;
+    int segment_mod = y_dimension % thread_count;
+
+    int segment_start =0;
+    for( int current_thread = 0 ; current_thread < thread_count; ++current_thread){ 
+        if(segment_mod > 0){
+            segment_start=  current_thread * segment_size +1;
+            segment_size_adj = segment_size + 1;
+            --segment_mod;
+        }else{
+            segment_start=  current_thread * segment_size;
+            segment_size_adj = segment_size;
+        }
+        /*
+            const int segment_start, // start of segment 
+            const int segment_size, // segment size
+            const int mask_size, // size of mask
+            T** mask, // mask
+            const int y_dimension, // image height
+            const int x_dimension, // image width
+            const  std::vector<unsigned char> &original,
+            std::vector<unsigned char> &filtered
+         */
+        thread_list.push_back( 
+            std::thread(apply_mask,segment_start,
+                        segment_size_adj, mask_size,
+                        mask,
+                        y_dimension,
+                        x_dimension,
+                        image,
+                        fi)
+            );
+    }
+    
+    //apply_mask(mask_size,mask,y_dimension, x_dimension,image, fi);
+    for(int i = 0 ; i < thread_count; ++i){
+        thread_list[i].join();
+    }
+    std::cout<<"Saving..."<<std::endl;
+    UWM::PGM().write(file_path_new,fi , x_dimension, y_dimension);
     // Clean up
     for(int i = 0; i < mask_size; i++){
        delete mask[i];
