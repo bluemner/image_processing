a5c1c22ea67f06c0c7fc93951edc28cc816742a5
Beta <beatcore-org@users.noreply.github.com>
2017-10-24 12:19:53 -0500

    working both filters :)

--- source/hw2.cpp
+++ source/hw2.cpp
@@ -5,39 +5,8 @@
 #include <cmath>
 #include "../headers/PGM.hpp"
 #define T double
+#define B 256
 
-void mean(
-    const int y_dimension,
-    const int x_dimension,
-    unsigned char * image,
-    unsigned char &result
-    )
-    {
-    unsigned int sum =0;
-    for(int i=0; i< y_dimension; ++i){
-        for(int j=0; j<x_dimension; ++j){
-            sum+= image[j * x_dimension + i];
-        }
-    }
-    result = (unsigned char)   sum / (unsigned char)  (y_dimension * x_dimension);
-}
-
-void variance(
-    const int mean,
-    const int y_dimension,
-    const int x_dimension,
-    unsigned char * image,
-    unsigned char &result
-    )
-{
-    unsigned int sum =0;
-    for(int i=0; i< y_dimension; ++i){
-        for(int j=0; j<x_dimension; ++j){
-            sum+= (image[j * x_dimension + i]-mean) *(image[j * x_dimension + i]-mean) ;
-        }
-    }
-    result = (unsigned char)  sum / (unsigned char)   (y_dimension * x_dimension);
-}
 
 void apply_mask(const int segment_start, // start of segment 
     const int segment_size, // segment size
@@ -84,6 +53,8 @@ void apply_mask(const int segment_start, // start of segment
     }
     std::cout << "Exiting thread" <<std::endl;
 }
+
+
 void get_gaussian_mask(const int size, T** mask)
 {
     //               5 /2 = 2; 
@@ -109,6 +80,77 @@ void get_gaussian_mask(const int size, T** mask)
     }
         
 }
+void apply_bilateral_filtering_mask(
+    const int segment_start, // start of segment 
+    const int segment_size, // segment size
+    const int mask_size, // size of mask
+    T** mask, // mask
+    const int mask_2_size,
+    T* mask_2,
+    const int y_dimension, // image height
+    const int x_dimension, // image width
+    unsigned char * original,
+    unsigned char * filtered)
+{
+    int k_shift = 0;
+    int l_shift = 0;
+    T temp_0 =0;
+    T temp_1 =0;
+
+    for(int i =0; i<x_dimension; i++){
+        for(int j=segment_start; j< segment_size; j++){
+             for(int k=0; k <  mask_size; k++){
+                for(int l=0;l <  mask_size; l++){
+
+                    //filtered[j*image_width+ i] = mask[k-i][l-j] *filtered[j*image_width+ i];
+                    k_shift = i - k / 2;
+                    l_shift = j - l / 2;
+                    if( k_shift < 0 || k_shift >= x_dimension){
+                        k_shift=0;
+                    }
+                    if( l_shift < 0 || l_shift >=y_dimension ){
+                        l_shift=0;
+                    }
+                    int abs_ps = (int) abs(original[l_shift*x_dimension+k_shift ]- original[j * x_dimension + i]);
+                    // filtered[j*x_dimension+ i] += mask[l][k] 
+                    //                               * original[l_shift*x_dimension+k_shift ]
+                    //                               * mask_2[abs_ps];
+                   
+                    temp_0 += mask[l][k] * original[l_shift*x_dimension+k_shift ] ;
+                    temp_1 += mask[l][k] ; // mask_2[abs_ps]
+                }
+            }
+
+            filtered[j*x_dimension+ i] =  (unsigned char) floor(temp_0 / temp_1); 
+            temp_0 =0;
+            temp_1 =0;
+            if(i+1 == x_dimension && j+1==segment_size )
+                std::cout<<"Done with Row::"<< j << std::endl;
+        }
+
+    }
+    std::cout << "Exiting thread" <<std::endl;
+}
+
+void bilateral_filtering_mask(const int mask_1_size, T** mask_1, const int mask_2_size , T* mask_2)
+{
+    //               5 /2 = 2; 
+    int mp =(int)  mask_1_size / 2;
+    T sigma_s = 1.0, sigma_r =1.0;
+    T rs = 2.0 * sigma_r * sigma_s; 
+    T s = 2.0 * sigma_s * sigma_s;
+    T sum = 0.0;
+    std::cout<< "mp:\t" <<mp<< "\t"<< (-1* mp) << std::endl;
+    for(int i = (-1* mp); i<= mp; i++){
+        for(int j = (-1* mp); j<= mp; j++){
+          mask_1[i+mp][j+mp] =  exp(-(i*i+j*j)/(s));
+        }
+    }
+    for(int i=0; i<mask_2_size; i++){
+        mask_2[i] = exp(-((i)/rs));
+    }
+
+}
 void bilateral_filtering(
     const int segment_start,
     const int segment_size,
@@ -118,7 +160,7 @@ void bilateral_filtering(
     unsigned char * original,
     T ** mask)
 {
-    T sigma = 1.0;
+    T sigma = 4.0;
     T s_d = 2.0 * sigma * sigma; 
     T s_r = 2.0 * sigma * sigma; 
     
@@ -218,6 +260,8 @@ int main(int argc, char * argv[]){
             mask[i][j] = (T) 0.0;
         }
     }
+    int mask_2_size = B;
+    T* mask_2 = new T[mask_2_size]; // This should be change if using more then 256 for storage
     
     get_gaussian_mask(mask_size, mask);
     print(mask_size,mask);
@@ -250,10 +294,11 @@ int main(int argc, char * argv[]){
                         y_dimension,
                         x_dimension,
                         test_image,
-                        fi)
+                        gi)
             );
-            segment_start = segment_size_adj;
+        segment_start = segment_size_adj;
     }
+    
     //apply_mask(0,y_dimension , mask_size,mask,y_dimension, x_dimension,test_image, fi);
     /*
         bilateral_filtering(
@@ -265,21 +310,33 @@ int main(int argc, char * argv[]){
     unsigned char * original,
     unsigned char * filtered)
     */
-
-    bilateral_filtering(0,
-        y_dimension,
-        mask_size,
-        y_dimension, // image height
-        x_dimension, // image width
-        test_image,
-        mask);
-
-    apply_mask(0,y_dimension , mask_size,mask,y_dimension, x_dimension,test_image, gi);
-    
     for(int i = 0 ; i < thread_list.size(); ++i){
         thread_list[i].join();
     }
 
+    for(int i = 0; i < mask_size; i++){
+        for(int j=0; j<mask_size; j++){
+            mask[i][j] = (T) 0.0;
+        }
+    }
+    std::cout<<"BI"<<std::endl;
+    bilateral_filtering_mask(mask_size, mask, mask_2_size , mask_2);
+    print(mask_size,mask);
+    /*
+    const int segment_start, // start of segment 
+    const int segment_size, // segment size
+    const int mask_size, // size of mask
+    T** mask, // mask
+    const int mask_2_size,
+    T* mask_2,
+    const int y_dimension, // image height
+    const int x_dimension, // image width
+    unsigned char * original,
+    unsigned char * filtered
+    */
+    apply_bilateral_filtering_mask(0,y_dimension , mask_size,mask, mask_2_size, mask_2,y_dimension, x_dimension,test_image, fi);
+    
+   
     std::cout<<"Saving..."<<std::endl;
 
     UWM::PGM().write(file_path_new_bilateral_filtering,fi  , x_dimension, y_dimension);
@@ -290,6 +347,7 @@ int main(int argc, char * argv[]){
        delete mask[i];
     }
     delete mask;
+    delete mask_2;
     delete fi;
     delete gi;
     return 0;
